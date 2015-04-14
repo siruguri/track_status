d2a1e6da387986c258e650a08689b662e567d290
Sameer Siruguri <siruguri@gmail.com>
2015-04-14 10:35:26 -0700

    Removed bad test file

--- app/services/test_readability_parser.rb
+++
@@ -1,6 +0,0 @@
-require 'readability_parser'
-require_relative './readability_parser_wrapper'
-
-k = ReadabilityParserWrapper.new.parse 'http://www.the-tls.co.uk/tls/public/article1541210.ece'
-
-puts k.content
