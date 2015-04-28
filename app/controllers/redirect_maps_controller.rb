bb6da6376395d50706a6d9ab2e8b11670eb30d63
Sameer Siruguri <siruguri@gmail.com>
2015-04-27 18:28:29 -0700

    Store redirects in database

--- app/controllers/redirect_maps_controller.rb
+++ app/controllers/redirect_maps_controller.rb
@@ -2,6 +2,9 @@ class RedirectMapsController < ApplicationController
   def show
     src = params[:id]
     if r=RedirectMap.find_by_src(src)
+      req = RedirectRequest.new(request_referer: request.referer, request_agent: request.user_agent)
+      req.redirect_map = r
+      req.save
       redirect_to r.dest
     else
       render nothing: true
---
+++ test/controllers/redirect_maps_controller_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class RedirectMapsControllerTest < ActionController::TestCase
+  def setup
+    @map = redirect_maps(:one)
+  end
+  
+  test 'getting a redirect updates the DB' do
+    assert_difference('RedirectRequest.count', 1) do
+      get :show, {id: @map.src}
+    end
+    
+    assert_redirected_to @map.dest
+
+    req = RedirectRequest.last
+    assert_equal 'Rails Testing', req.request_agent
+  end
+end
