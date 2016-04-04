f62d86d649ddf04063f98545d7628fc53eb22ce8
Sameer Siruguri <siruguri@gmail.com>
2016-04-04 13:42:01 -0700

    Twitter app: change view formats

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema.define(version: 20160127064228) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "hstore"
   enable_extension "citext"
 
   create_table "account_entries", force: :cascade do |t|
